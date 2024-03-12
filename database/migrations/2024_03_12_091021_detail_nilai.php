e8d3bba0cb454407e42fca21ecaa2d56319f9f21
rafasepti <rafasepti@gmail.com>
2024-03-12 16:12:21 +0700

    add: migration detail_nilai

---
+++ database/migrations/2024_03_12_091021_detail_nilai.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_nilai', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_nilai');
+            $table->integer('id_guru');
+            $table->integer('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_nilai');
+    }
+};
