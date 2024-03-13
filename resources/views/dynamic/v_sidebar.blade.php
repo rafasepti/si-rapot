fb93f1d3168560b77d7982da1a2d287000a752c1
rafasepti <rafasepti@gmail.com>
2024-03-13 19:19:50 +0700

    add: view mapel

--- resources/views/dynamic/v_sidebar.blade.php
+++ resources/views/dynamic/v_sidebar.blade.php
@@ -17,6 +17,13 @@
       </li>
 
       <li class="nav-item">
+        <a class="nav-link collapsed" href="/mapel">
+          <i class="bi bi-book-fill"></i>
+          <span>Mata Pelajaran</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
---
+++ resources/views/mapel/data_mapel.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<body>
+  @include('dynamic/v_header');
+  @include('dynamic/v_sidebar');
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Mata Pelajaran</h1>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Data Mata Pelajaran</h5>
+                <!-- Table with stripped rows -->
+                <table class="table datatable">
+                  <thead>
+                    <tr>
+                      <th>No.</th>
+                      <th>Nama Mapel</th>
+                      <th>KKM</th>
+                      <th>Aksi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Unity Pugh</td>
+                      <td>9958</td>
+                      <td>Curicó</td>
+                      <td>2005/02/11</td>
+                      <td style="text-align: center">
+                        <a href="/siswa/detail/" class="btn btn-outline-info">
+                          <i class="bi bi-info-lg"></i>
+                        </a>
+                        <a href="/siswa/edit/" class="btn btn-outline-success">
+                          <i class="bi bi-pencil-square"></i>
+                        </a>
+                        <a href="/siswa/hapus/" class="btn btn-outline-danger" onclick="return confirm('Apakah anda yakin?')">
+                          <i class="bi bi-trash-fill"></i>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <!-- End Table with stripped rows -->
+              </div>
+            </div>
+        </div><!-- End col-md -->
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  @include('dynamic/v_footer');
+
+</body>
+
+</html>
