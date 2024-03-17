139f679792215eb317e714f56be2c552a75a27e3
rafasepti <rafasepti@gmail.com>
2024-03-17 17:00:55 +0700

    create: breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,58 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin/v_admin');
+    return view('welcome');
 });
 
-//siswa
-Route::get('/siswa', 'App\Http\Controllers\SiswaController@index');
-Route::get('/siswa/list', 'App\Http\Controllers\SiswaController@siswaGet');
-Route::get('/siswa/tambah', 'App\Http\Controllers\SiswaController@create');
-Route::post('/siswa/tambah', 'App\Http\Controllers\SiswaController@store');
-Route::get('/siswa/detail/{id}', 'App\Http\Controllers\SiswaController@detail');
-Route::get('/siswa/edit/{id}','App\Http\Controllers\SiswaController@edit');
-Route::post('/siswa/update','App\Http\Controllers\SiswaController@update');
-Route::get('/siswa/hapus/{id}','App\Http\Controllers\SiswaController@destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//mapel
-Route::get('/mapel', 'App\Http\Controllers\MapelController@index');
-Route::get('/mapel/list', 'App\Http\Controllers\MapelController@mapelGet');
-Route::get('/mapel/detail/{id}', 'App\Http\Controllers\MapelController@detail');
-Route::get('/mapel/tambah', 'App\Http\Controllers\MapelController@create');
-Route::post('/mapel/store', 'App\Http\Controllers\MapelController@store');
-Route::get('/mapel/edit/{id}','App\Http\Controllers\MapelController@edit');
-Route::post('/mapel/update','App\Http\Controllers\MapelController@update');
-Route::get('/mapel/hapus/{id}','App\Http\Controllers\MapelController@destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//tahun ajaran
-Route::get('/tahunajaran', 'App\Http\Controllers\TahunAjaranController@index');
-Route::get('/tahunajaran/list', 'App\Http\Controllers\TahunAjaranController@tahunGet');
-Route::get('/tahunajaran/tambah', 'App\Http\Controllers\TahunAjaranController@create');
-Route::post('/tahunajaran/store', 'App\Http\Controllers\TahunAjaranController@store');
-Route::get('/tahunajaran/edit/{id}','App\Http\Controllers\TahunAjaranController@edit');
-Route::post('/tahunajaran/update','App\Http\Controllers\TahunAjaranController@update');
-Route::get('/tahunajaran/hapus/{id}','App\Http\Controllers\TahunAjaranController@destroy');
-
-//guru
-Route::get('/guru', 'App\Http\Controllers\GuruController@index');
-Route::get('/guru/list', 'App\Http\Controllers\GuruController@guruGet');
-Route::get('/guru/tambah', 'App\Http\Controllers\GuruController@create');
-Route::get('/guru/detail/{id}', 'App\Http\Controllers\GuruController@detail');
-Route::post('/guru/store', 'App\Http\Controllers\GuruController@store');
-Route::get('/guru/edit/{id}','App\Http\Controllers\GuruController@edit');
-Route::post('/guru/update','App\Http\Controllers\GuruController@update');
-Route::get('/guru/hapus/{id}','App\Http\Controllers\GuruController@destroy');
-
-//Kepala Sekolah
-Route::get('/kepsek', 'App\Http\Controllers\GuruController@indexKS');
-Route::post('/kepsek', 'App\Http\Controllers\GuruController@storeKS');
-Route::get('/kepsek/loadKepsek', 'App\Http\Controllers\GuruController@loadKepsek');
-
-//kelas
-Route::get('/kelas', 'App\Http\Controllers\KelasController@index');
-Route::get('/kelas/list', 'App\Http\Controllers\KelasController@kelasGet');
-Route::get('/kelas/tambah', 'App\Http\Controllers\KelasController@create');
-Route::post('/kelas/store', 'App\Http\Controllers\KelasController@store');
-Route::get('/kelas/edit/{id}','App\Http\Controllers\KelasController@edit');
-Route::post('/kelas/update','App\Http\Controllers\KelasController@update');
-Route::get('/kelas/hapus/{id}','App\Http\Controllers\KelasController@destroy');
+require __DIR__.'/auth.php';
